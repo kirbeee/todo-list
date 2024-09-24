de63b63353e23ba47193eaa740be42eeb0f2cae1
Sean Lin <pspsean1234@gmail.com>
2024-09-24 18:18:00 +0800

    add event listener

---
+++ src/views/HeaderView.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-page-header>
+    <template #content>
+      <el-breadcrumb-item to="/">Home</el-breadcrumb-item>
+      <el-breadcrumb-item to="/todo-list">Todo List</el-breadcrumb-item>
+    </template>
+  </el-page-header>
+</template>
+<script lang="ts" setup>
+</script>
